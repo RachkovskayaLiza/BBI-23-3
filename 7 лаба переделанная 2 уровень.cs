be1cc913f5d9bdc25e412ea015259f1501831dae
RachkovskayaLiza <160212585+RachkovskayaLiza@users.noreply.github.com>
2024-04-08 13:04:09 +0300

    7 лаба переделанная

---
+++ 7 лаба переделанная 2 уровень.cs
@@ -0,0 +1,122 @@
+﻿class Diving
+{
+    protected string _Surname;
+    protected double[,] _Ball;
+    protected static string _Discipline = "";
+    protected string Discipline;
+
+
+    public string D => Discipline;
+    public string Surname => _Surname;
+
+    public double TotalBall
+    {
+        get
+        {
+            double sum = 0;
+            for (int i = 0; i < 5; i++)
+            {
+                sum += _Ball[0, i] + _Ball[1, i];
+            }
+            return sum;
+        }
+    }
+    public Diving(string surname, double[,] Ball)
+    {
+        _Surname = surname;
+        _Ball = Ball;
+    }
+    public virtual void Print()
+    {
+        Console.WriteLine("Дисциплина: {0}", Discipline);
+
+
+        Console.Write("Имя   | Оценка1  | Оценка2 | Оценка3 | Оценка4 | Оценка5 | Total\n");
+        Console.Write(_Surname + " ");
+
+        for (int j = 0; j < 5; j++)
+        {
+            Console.Write("{0}({1})   | ", _Ball[0, j], _Ball[1, j]);
+        }
+        Console.WriteLine("{0}", CalculateTotalBall());
+
+    }
+    private double CalculateTotalBall()
+    {
+        double sum = 0;
+        for (int i = 0; i < 5; i++)
+        {
+            sum += _Ball[0, i] + _Ball[1, i];
+        }
+        return sum;
+    }
+
+
+
+}
+class ThreeMeterDiving : Diving
+{
+    public ThreeMeterDiving(string surname, double[,] Ball) : base(surname, Ball)
+    {
+        _Discipline = "3 meter";
+        Discipline = _Discipline;
+    }
+
+}
+
+class FiveMeterDiving : Diving
+{
+
+    public FiveMeterDiving(string surname, double[,] Ball) : base(surname, Ball)
+    {
+        _Discipline = "5 meter";
+        Discipline = _Discipline;
+    }
+
+}
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        double[,] ball1 = { { 8.5, 8.0, 9.0, 9.5, 8.5 }, { 7.0, 8.5, 7.5, 8.0, 6.5 } };
+        double[,] ball2 = { { 7.5, 8.0, 7.5, 8.0, 7.0 }, { 6.5, 7.0, 6.0, 7.5, 6.0 } };
+        double[,] ball3 = { { 6.5, 7.0, 7.0, 7.5, 6.5 }, { 8.0, 7.5, 7.0, 8.5, 7.5 } };
+        double[,] ball4 = { { 7.0, 6.5, 8.0, 6.5, 8.5 }, { 8.0, 6.0, 7.5, 6.0, 7.0 } };
+        double[,] ball5 = { { 8.0, 8.5, 7.5, 7.0, 8.0 }, { 6.0, 6.5, 7.0, 6.5, 7.5 } };
+
+        Diving[] divers1 = new Diving[5]
+        {
+            new ThreeMeterDiving("Прыгун1", ball1),
+            new ThreeMeterDiving("Прыгун2", ball2),
+            new ThreeMeterDiving("Прыгун3", ball3),
+            new FiveMeterDiving("Прыгун4", ball4),
+            new FiveMeterDiving("Прыгун5", ball5)
+        };
+
+        Sort(divers1);
+
+        Console.WriteLine("Итоговый протокол соревнований:");
+
+        for (int i = 0; i < divers1.Length; i++)
+        {
+            divers1[i].Print();
+        }
+        Console.ReadKey();
+    }
+    static void Sort(Diving[] divers)
+    {
+        for (int i = 0; i < divers.Length - 1; i++)
+        {
+            for (int j = i + 1; j < divers.Length; j++)
+            {
+                if (divers[j].D.CompareTo(divers[i].D) < 0)
+                {
+                    (divers[j], divers[i]) = (divers[i], divers[j]);
+                }
+            }
+        }
+    }
+
+}
+
---
+++ 7 лаба переделанная 3 уровень.cs
@@ -0,0 +1,104 @@
+﻿public class Team
+{
+    protected int[] results;
+
+    public Team(int[] results)
+    {
+        this.results = results;
+    }
+
+    public virtual int SumPoints()
+    {
+        int points = 0;
+        foreach (int result in results)
+        {
+            switch (result)
+            {
+                case 1:
+                    points += 5;
+                    break;
+                case 2:
+                    points += 4;
+                    break;
+                case 3:
+                    points += 3;
+                    break;
+                case 4:
+                    points += 2;
+                    break;
+                case 5:
+                    points += 1;
+                    break;
+                default:
+                    points += 0;
+                    break;
+            }
+        }
+        return points;
+    }
+}
+
+public class FemaleTeam : Team
+{
+    public FemaleTeam(int[] results) : base(results) { }
+}
+
+public class MaleTeam : Team
+{
+    public MaleTeam(int[] results) : base(results) { }
+}
+
+class Program
+{
+    static void Main()
+    {
+        int[] resultsTeam1m = { 1, 2, 3, 4, 5, 6 };
+        int[] resultsTeam2m = { 7, 8, 9, 10, 11, 12 };
+        int[] resultsTeam3m = { 13, 14, 15, 16, 17, 18 };
+        int[] resultsTeam4w = { 1, 3, 4, 7, 10, 12 };
+        int[] resultsTeam5w = { 2, 5, 6, 8, 9, 11 };
+        int[] resultsTeam6w = { 17, 16, 15, 14, 13, 18 };
+
+
+        Team[] femaleTeam ={
+            new FemaleTeam(resultsTeam6w),
+             new FemaleTeam(resultsTeam4w),
+              new FemaleTeam(resultsTeam5w)
+        };
+        Team[] maleTeam ={
+            new MaleTeam(resultsTeam1m),
+            new MaleTeam(resultsTeam3m),
+            new MaleTeam(resultsTeam2m)
+        };
+
+
+
+        int bestFemaleScore = BestScore(femaleTeam)[0];
+        int bestMenScore = BestScore(maleTeam)[0];
+
+        Team winningTeam = bestFemaleScore > bestMenScore ? femaleTeam[BestScore(femaleTeam)[1]] : maleTeam[BestScore(maleTeam)[1]];
+
+
+        Console.WriteLine($"команда победителей набрала  {winningTeam.SumPoints()} баллов.");
+
+        Console.ReadKey();
+    }
+    static int[] BestScore(Team[] a)
+    {
+        int max = 0;
+        int imax = 0;
+        for (int i = 0; i < a.Length; i++)
+        {
+
+            if (a[i].SumPoints() > max)
+            {
+                max = a[i].SumPoints();
+                imax = i;
+            }
+
+        }
+        int[] b = { max, imax };
+        return b;
+    }
+}
+
